ca9ddfe9fce8341920ceb676dc97bbe93735cbfd
Đức Duy - zuydd <ducduy.spam@gmail.com>
2024-09-08 19:39:04 +0700

    feat: set max retry

--- src/run/index.js
+++ src/run/index.js
@@ -1,5 +1,7 @@
 import colors from "colors";
+import dayjs from "dayjs";
 import delayHelper from "../helpers/delay.js";
+import fileHelper from "../helpers/file.js";
 import authService from "../services/auth.js";
 import dailyService from "../services/daily.js";
 import farmingClass from "../services/farming.js";
@@ -9,10 +11,16 @@ import taskService from "../services/task.js";
 import tribeService from "../services/tribe.js";
 import userService from "../services/user.js";
 
-// Điều chỉnh khoảng cách thời gian chạy vòng lặp đầu tiên giữa 2 tài khoản tránh bị spam request (tính bằng giây)
+// Điều chỉnh khoảng cách thời gian chạy vòng lặp đầu tiên giữa các luồng tránh bị spam request (tính bằng giây)
 const DELAY_ACC = 30;
+// Đặt số lần thử kết nối lại tối đa khi proxy lỗi, nếu thử lại quá số lần cài đặt sẽ dừng chạy tài khoản đó và ghi lỗi vào file log
+const MAX_RETRY_PROXY = 20;
+// Đặt số lần thử đăng nhập tối đa khi đăng nhập lỗi, nếu thử lại quá số lần cài đặt sẽ dừng chạy tài khoản đó và ghi lỗi vào file log
+const MAX_RETRY_LOGIN = 20;
 
 const run = async (user) => {
+  let countRetryProxy = 0;
+  let countRetryLogin = 0;
   await delayHelper.delay((user.index - 1) * DELAY_ACC);
   while (true) {
     // Kiểm tra kết nối proxy
@@ -23,18 +31,51 @@ const run = async (user) => {
         user.log.logError(
           "Proxy lỗi, kiểm tra lại kết nối proxy, sẽ thử kết nối lại sau 30s"
         );
-        await delayHelper.delay(30);
+        countRetryProxy++;
+        if (countRetryProxy >= MAX_RETRY_PROXY) {
+          break;
+        } else {
+          await delayHelper.delay(30);
+        }
       } else {
+        countRetryProxy = 0;
         isProxyConnected = true;
       }
+    }
+    try {
+      if (countRetryProxy >= MAX_RETRY_PROXY) {
+        const dataLog = `[No ${user.index} _ ID: ${
+          user.info.id
+        } _ Time: ${dayjs().format(
+          "YYYY-MM-DDTHH:mm:ssZ[Z]"
+        )}] Lỗi kết nối proxy - ${user.proxy}`;
+        fileHelper.writeLog("log.error.txt", dataLog);
+        break;
+      }
+
+      if (countRetryLogin >= MAX_RETRY_LOGIN) {
+        const dataLog = `[No ${user.index} _ ID: ${
+          user.info.id
+        } _ Time: ${dayjs().format(
+          "YYYY-MM-DDTHH:mm:ssZ[Z]"
+        )}] Lỗi đăng nhập thất bại quá ${MAX_RETRY_LOGIN} lần`;
+        fileHelper.writeLog("log.error.txt", dataLog);
+        break;
+      }
+    } catch (error) {
+      user.log.logError("Ghi lỗi thất bại");
     }
 
     // Đăng nhập tài khoản
     const login = await authService.handleLogin(user);
     if (!login.status) {
+      countRetryLogin++;
       await delayHelper.delay(60);
       continue;
+    } else {
+      countRetryLogin = 0;
     }
+
     await dailyService.checkin(user);
     await tribeService.handleTribe(user);
     await taskService.handleTask(user);

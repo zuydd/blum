3e027a2fba59965562819920774c002713672bc4
Đức Duy - zuydd <ducduy.spam@gmail.com>
2024-10-23 00:24:18 +0700

    update v0.1.8

--- src/services/user.js
+++ src/services/user.js
@@ -3,6 +3,8 @@ import he from "he";
 import { parse } from "querystring";
 import fileHelper from "../helpers/file.js";
 import { LogHelper } from "../helpers/log.js";
+import deviceService from "./device.js";
+import fakeService from "./fake.js";
 import { HttpService } from "./http.js";
 import server from "./server.js";
 
@@ -12,12 +14,17 @@ class UserService {
   async loadUser() {
     const rawUsers = fileHelper.readFile("users.txt");
     const rawProxies = fileHelper.readFile("proxy.txt");
+    const rawDevices = fileHelper.readFile("device.txt");
 
     const users = rawUsers
       .split("\n")
       .map((line) => line.trim())
       .filter((line) => line.length > 0);
     const proxies = rawProxies
+      .split("\n")
+      .map((line) => line.trim())
+      .filter((line) => line.length > 0);
+    const devices = rawDevices
       .split("\n")
       .map((line) => line.trim())
       .filter((line) => line.length > 0);
@@ -34,8 +41,17 @@ class UserService {
         const userParse = parse(he.decode(decodeURIComponent(user)));
         const info = JSON.parse(userParse.user);
         const proxy = proxies[index] || null;
+        // handle device
+        let device = devices.find(
+          (d) => d.split("|")[0] === info.id.toString()
+        );
+        if (!device) {
+          device = fakeService.createDeviceInfo(info.id, 0);
+          fileHelper.writeLog("device.txt", device);
+        }
+        const deviceInfo = deviceService.initDataDevice(device);
         const log = new LogHelper(index + 1, info.id);
-        const http = new HttpService(log, proxy);
+        const http = new HttpService(log, deviceInfo, proxy);
         let query_id = user;
         if (user && user.includes("query_id%3D")) {
           query_id = he.decode(decodeURIComponent(query_id));
